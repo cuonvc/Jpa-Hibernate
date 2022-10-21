be79e87693bd7ee285c1e11e0150ff79424900ab
cuonvc <cuong.fithau@gmail.com>
2022-10-21 18:31:43 +0700

    merger method

--- src/main/java/com/lean/jpahibernate/JpaHibernateApplication.java
+++ src/main/java/com/lean/jpahibernate/JpaHibernateApplication.java
@@ -33,15 +33,30 @@ public class JpaHibernateApplication implements CommandLineRunner {
 
 		transaction.begin();  //start transaction - connected to DB
 
-		Person person = session.get(Person.class, 10);  //get person with id=10
-		person.setFirstName("test first update");
-		person.setLastName("test last update");
+		Person person = session.get(Person.class, 12);  //get person with id=10
+		transaction.commit();  //commit to DB
+		session.close();
+		person.setAge(10);  //error vì session đã closed
+
+		Session session1 = sessionFactory.openSession();
+		Transaction transaction1 = session1.getTransaction();
+		transaction1.begin();
+		Person person1 = session1.get(Person.class, 12);
+		person1.setFirstName("update 1");
+		person1.setLastName("update 2");
+		session1.merge(person);   //update lại dữ liệu từ session trước
+		transaction1.commit();
+		session1.close();
+
+		//note: mặc dù person có id=12 được commit và close session ngay sau khi get lên persistent nhưng
+		//trạng thái của chúng vẫn nằm trong persistent. Vậy nên từ lần mở sessiton tiếp theo, nếu muốn thay
+		//đổi data thì chỉ cần dùng hàm merge thay cho update
 
 //		session.delete(person);  //remove person from persistent state
-		session.save(person);   // to persistent state (update person on persistent state)
-
-		transaction.commit();  //commit to DB
-
-		session.close();
+//		session.save(person);   // to persistent state (update person on persistent state)
+//
+//		transaction.commit();  //commit to DB
+//
+//		session.close();
 	}
 }
